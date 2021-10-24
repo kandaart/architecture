1e514c7651fc6b6ac78e0dfa6b961c714cff972d
Artem Kandaurov <artem@kandaurov.net>
2021-10-24 13:57:10 +0200

    Sync develop with master
    * SPM instead of Carthage DM
    
    * Package.swift instead of Xcode project
    
    * Ignore swiftpm folder
    
    * Revert ignore swiftpm folder
    
    * Update README

--- bootstrap_carthage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-./carthage.sh bootstrap --platform ios --no-use-binaries --cache-builds
-osascript -e 'display notification "Carthage bootstrap is done" with title "Carthage"'
--- update_carthage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-./carthage.sh update --platform iOS --no-use-binaries --cache-builds --new-resolver
-osascript -e 'display notification "Carthage update is done" with title "Carthage"'
